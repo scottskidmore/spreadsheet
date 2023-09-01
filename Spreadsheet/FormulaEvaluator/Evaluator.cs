1c4c68f62f67ff25b8fe48d5a9bba95166494d11
scottskidmore <scottytskidmore@gmail.com>
2023-09-01 15:57:30 -0600

    Formula Evaluator Code

---
+++ Spreadsheet/FormulaEvaluator/Evaluator.cs
@@ -0,0 +1,287 @@
+﻿using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+
+namespace FormulaEvaluator;
+/// <summary>
+/// Class that provides the methods required to evaluate an arithmetic expressions using standard infix notation.
+/// </summary>
+public static class Evaluator
+{
+    /// <summary>
+    /// Delegate that will retrieve the value of a reference provided in the equation.
+    /// </summary>
+    /// <param name="v">The reference value in the equation.</param>
+    /// <returns>An integer value of the reference value.</returns>
+    public delegate int Lookup(String v);
+
+    /// <summary>
+    /// Method for evaluating the integer value of an arithmetic expressions using standard infix notation.
+    /// </summary>
+    /// <param name="exp">The expression to be evaluated.</param>
+    /// <param name="variableEvaluator">The delegate that will return the value of a reference value.</param>
+    /// <returns></returns>
+    /// <exception cref="InvalidDataException"></exception>
+    public static int Evaluate(String exp, Lookup variableEvaluator)
+    {
+        string trimmed = String.Concat(exp.Where(c => !Char.IsWhiteSpace(c)));
+        string[] substrings = Regex.Split(trimmed, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
+        Stack<int> values = new Stack<int>();
+        Stack<string> op = new Stack<string>();
+        foreach (string s in substrings)
+        {
+            string type = stringType(s);
+            switch (type)
+            {
+                case "Integer":
+                    if (topStack(op) is "/" or "*")
+                    {
+                        string operate = op.Pop();
+                        if (values.Count <= 0)
+                        {
+                            throw new ArgumentException("equation format out of order.");
+                        }
+                        int value = values.Pop();
+                        if (int.Parse(s) == 0)
+                        {
+                            throw new ArgumentException("Invalid 0 division.");
+                        }
+                        if (operate == "/")
+                        {
+                            values.Push(value / int.Parse(s));
+                        }
+                        if (operate == "*")
+                        {
+                            values.Push(value * int.Parse(s));
+                        }
+                    }
+                    else
+                    {
+                        values.Push(int.Parse(s));
+                    }
+                    break;
+                case "Reference":
+                    if (topStack(op) is "/" or "*")
+                    {
+                        string operate = op.Pop();
+                        if (values.Count <= 0)
+                        {
+                            throw new ArgumentException("equation format out of order.");
+                        }
+                        int value = values.Pop();
+                        if (variableEvaluator(s) == 0)
+                        {
+                            throw new ArgumentException("Invalid 0 division.");
+                        }
+                        if (operate == "/")
+                        {
+                            values.Push(value / variableEvaluator(s));
+                        }
+                        if (operate == "*")
+                        {
+                            values.Push(value * variableEvaluator(s));
+                        }
+                    }
+                    else
+                    {
+                        values.Push(variableEvaluator(s));
+                    }
+                    break;
+                case "Add":
+                    if (topStack(op) is "+" or "-")
+                    {
+                        string operate = op.Pop();
+                        if (values.Count <= 1)
+                        {
+                            throw new ArgumentException("equation format out of order.");
+                        }
+                        int value = values.Pop();
+                        int value2 = values.Pop();
+                        if (operate == "+")
+                        {
+                            values.Push(value2 + value);
+                        }
+                        if (operate == "-")
+                        {
+                            values.Push(value2 - value);
+                        }
+                    }
+                    op.Push(s);
+                    break;
+                case "Subtract":
+                    if (topStack(op) is "+" or "-")
+                    {
+                        string operate = op.Pop();
+                        if (values.Count <= 1)
+                        {
+                            throw new ArgumentException("equation format out of order.");
+                        }
+                        int value = values.Pop();
+                        int value2 = values.Pop();
+                        if (operate == "+")
+                        {
+                            values.Push(value2 + value);
+                        }
+                        if (operate == "-")
+                        {
+                            values.Push(value2 - value);
+                        }
+                    }
+                        op.Push(s);
+                    
+                    break;
+                case "Multiply":
+                    op.Push(s);
+                    break;
+                case "Divide":
+                    op.Push(s);
+                    break;
+                case "LeftParenthesis":
+                    op.Push(s);
+                    break;
+                case "RightParenthesis":
+                    if (topStack(op) is "+" or "-")
+                    {
+                        string operate = op.Pop();
+                        if (values.Count <= 1)
+                        {
+                            throw new ArgumentException("equation format out of order.");
+                        }
+                        int value = values.Pop();
+                        int value2 = values.Pop();
+                        if (operate == "+")
+                        {
+                            values.Push(value2 + value);
+                        }
+                        if (operate == "-")
+                        {
+                            values.Push(value2 - value);
+                        }
+                        if (topStack(op) != "(")
+                        {
+                            throw new ArgumentException("equation format out of order.");
+                        }
+                        op.Pop();
+
+                    }
+                    else if (topStack(op) == "(")
+                    {
+                        op.Pop();
+                    }
+                    if (topStack(op) is "/" or "*")
+                    {
+                        string operate = op.Pop();
+                        if (values.Count <= 1)
+                        {
+                            throw new ArgumentException("equation format out of order.");
+                        }
+                        int value = values.Pop();
+                        int value2 = values.Pop();
+                        if (value == 0)
+                        {
+                            throw new ArgumentException("Invalid 0 division.");
+                        }
+                        if (operate == "*")
+                        {
+                            values.Push(value2 * value);
+                        }
+                        if (operate == "/")
+                        {
+                            values.Push(value2 / value);
+                        }
+
+                      
+               
+                    }
+                    
+
+
+                    break;
+            }
+                    }
+        if (op.Count <= 0&&values.Count==1)
+        {
+            return values.Pop();
+        }
+        if(op.Count==1&&values.Count==2)
+        {
+            string operate = op.Pop();
+            if (values.Count <= 1)
+            {
+                throw new ArgumentException("equation format out of order.");
+            }
+            int value = values.Pop();
+            int value2 = values.Pop();
+            if (operate == "+")
+            {
+                values.Push(value2 + value);
+            }
+            if (operate == "-")
+            {
+                values.Push(value2 - value);
+            }
+            return values.Pop();
+        }
+        throw new ArgumentException("equation format out of order.");
+    }
+        
+    /// <summary>
+    /// Helper method that finds the type of a character in the expression.
+    /// </summary>
+    /// <param name="s">The string representation of the character.</param>
+    /// <returns>The type of the character as a string.</returns>
+    public static string stringType(String s)
+    {
+        if( int.TryParse(s, out _))
+        {
+            return "Integer";
+        }
+        if (Regex.IsMatch(s, "^[A-Za-z]+[0-9]+$"))
+        {
+            return "Reference";
+        }
+        if (s == "+" )
+        {
+            return "Add";
+        }
+        if (s == "-")
+        {
+            return "Subtract";
+        }
+        if (s == "*")
+        {
+            return "Multiply";
+        }
+        if (s == "/")
+        {
+            return "Divide";
+        }
+        if (s == "(")
+        {
+            return "LeftParenthesis";
+        }
+        if (s == ")")
+        {
+            return "RightParenthesis";
+        }
+        else
+        {
+            return "";
+        }
+    }
+    /// <summary>
+    /// Helper method that returns the top value of a stack without removing it.
+    /// </summary>
+    /// <param name="s">The stack that needs to be checked.</param>
+    /// <returns>The value on the top of the stack.</returns>
+    public static string topStack(Stack<string> s)
+    {
+        if (s.Count <= 0){
+            return "";
+        }
+        else
+        {
+            return s.Peek();
+        }
+    }
+}
+
---
+++ Spreadsheet/SpreadsheetConsoleApp/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace SpreadsheetConsoleApp;
+/// <summary>
+/// Class for testing the evaluator methods.
+/// </summary>
+class Program
+{
+    /// <summary>
+    /// A test delegate created to reflect how one would really function.
+    /// </summary>
+    /// <param name="v">The string of the reference we are looking for.</param>
+    /// <returns>The integer value of a reference</returns>
+    public static int Lookup(string v)
+    {
+        if (v == "A1")
+        {
+            return 10;
+        }
+        if (v == "a2")
+        {
+            return 2;
+        }
+        else
+        {
+            return 5;
+        }
+    }
+    /// <summary>
+    /// Main method for running tests on the evaluator class.
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        //int var = FormulaEvaluator.Evaluator.Evaluate("(2+2)*2", null);
+        //Console.WriteLine("(2+2)*2"+" Expected:8 Got:"+var);
+        FormulaEvaluator.Evaluator.Lookup test = Lookup;
+        //int var1 = FormulaEvaluator.Evaluator.Evaluate("A1+5*2", test);
+        //Console.WriteLine("A1+5*2" + " Expected:20 Got:" + var1);
+        //int var2 = FormulaEvaluator.Evaluator.Evaluate("5/(a2-1)", test);
+        //Console.WriteLine("5/(a2-1)" + " Expected:5 Got:" + var2);
+        //int var3 = FormulaEvaluator.Evaluator.Evaluate("10/5", test);
+        //Console.WriteLine("10/5" + " Expected:2 Got:" + var3);
+        //int var4 = FormulaEvaluator.Evaluator.Evaluate("10/a2", test);
+        //Console.WriteLine("10/a2" + " Expected:5 Got:" + var4);
+        //int var5 = FormulaEvaluator.Evaluator.Evaluate("10-a2-5", test);
+        //Console.WriteLine("10-a2-5" + " Expected:3 Got:" + var5);
+        int var6 = FormulaEvaluator.Evaluator.Evaluate("(1*1)-2/2", test);
+        Console.WriteLine("(1*1)-2/2" + " Expected:2 Got:" + var6);
+        //int var7 = FormulaEvaluator.Evaluator.Evaluate("(10-a2)/0", test);
+        //Console.WriteLine("(10-a2)/0" + " Expected: Got:" + var7);
+    }
+}
+
